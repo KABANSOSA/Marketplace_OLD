68793f31d21ce3e091277a74e53854c1b2686f48
Bogdan <bogdan.zaicev07@gmail.com>
2025-06-07 12:01:33 +0300

    Optimize build process to reduce memory usage

--- next.config.js
+++ next.config.js
@@ -6,6 +6,36 @@ const nextConfig = {
   experimental: {
     optimizeCss: true,
     optimizePackageImports: ['@tanstack/react-query']
+  },
+  webpack: (config, { dev, isServer }) => {
+    // Оптимизации только для production сборки
+    if (!dev && !isServer) {
+      config.optimization = {
+        ...config.optimization,
+        minimize: true,
+        splitChunks: {
+          chunks: 'all',
+          minSize: 20000,
+          maxSize: 244000,
+          minChunks: 1,
+          maxAsyncRequests: 30,
+          maxInitialRequests: 30,
+          cacheGroups: {
+            defaultVendors: {
+              test: /[\\/]node_modules[\\/]/,
+              priority: -10,
+              reuseExistingChunk: true,
+            },
+            default: {
+              minChunks: 2,
+              priority: -20,
+              reuseExistingChunk: true,
+            },
+          },
+        },
+      }
+    }
+    return config
   }
 }
 
